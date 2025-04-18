5bb43bc8ece856129c7f01571ecfd4d5ac02e4c2
Joao <jfjoao12@gmail.com>
2025-04-18 12:56:09 -0500

    Re-added admin user

--- app/admin/customers.rb
+++ app/admin/customers.rb
@@ -1,18 +1,47 @@
 ActiveAdmin.register Customer do
+  permit_params :phone_id, :name, :phone_number, :address
 
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # Uncomment all parameters which should be permitted for assignment
-  #
-  # permit_params :phone_id, :name, :phone_number, :address
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:phone_id, :name, :phone_number, :address]
-  #   permitted << :other if params[:action] == 'create' && current_user.admin?
-  #   permitted
-  # end
-  
+  # Optional: customize the columns shown on the index page
+  index do
+    selectable_column
+    id_column
+    column :name
+    column :phone_number
+    column :phone             # shows linked Phone record
+    column :address
+    actions
+  end
+
+  # Optional: add filters in the sidebar
+  filter :name
+  filter :phone_model, as: :string, label: "Phone Model",
+         collection: proc { Phone.pluck(:model) }
+  filter :phone_number
+
+  # Customize the form for new/edit
+  form do |f|
+    f.semantic_errors
+    f.inputs do
+      f.input :phone, as: :select, collection: Phone.all.map { |p| [ p.model, p.id ] }
+      f.input :name
+      f.input :phone_number
+      f.input :address
+    end
+    f.actions
+  end
+
+  # Optional: show page customization
+  show do
+    attributes_table do
+      row :id
+      row :name
+      row :phone_number
+      row :address
+      row :phone do |customer|
+        link_to customer.phone.model, admin_phone_path(customer.phone)
+      end
+      row :created_at
+      row :updated_at
+    end
+  end
 end
--- app/admin/phones.rb
+++ app/admin/phones.rb
@@ -1,18 +1,49 @@
 ActiveAdmin.register Phone do
+  permit_params :phone_feature_id, :model, :color, :price, :storage_size, :imei_number
 
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # Uncomment all parameters which should be permitted for assignment
-  #
-  # permit_params :phone_feature_id, :model, :color, :price, :storage_size, :imei_number
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:phone_feature_id, :model, :color, :price, :storage_size, :imei_number]
-  #   permitted << :other if params[:action] == 'create' && current_user.admin?
-  #   permitted
-  # end
-  
+  index do
+    selectable_column
+    id_column
+    column :model
+    column :color
+    column :price
+    column :storage_size
+    column :imei_number
+    column :phone_feature    # shows linked PhoneFeature record
+    actions
+  end
+
+  filter :model
+  filter :color
+  filter :storage_size
+  filter :imei_number
+
+  form do |f|
+    f.semantic_errors
+    f.inputs do
+      f.input :phone_feature, as: :select, collection: PhoneFeature.all.map { |pf| [ pf.brand, pf.id ] }
+      f.input :model
+      f.input :color
+      f.input :price
+      f.input :storage_size
+      f.input :imei_number
+    end
+    f.actions
+  end
+
+  show do
+    attributes_table do
+      row :id
+      row :model
+      row :color
+      row :price
+      row :storage_size
+      row :imei_number
+      row :phone_feature do |phone|
+        link_to phone.phone_feature.brand, admin_phone_feature_path(phone.phone_feature)
+      end
+      row :created_at
+      row :updated_at
+    end
+  end
 end
